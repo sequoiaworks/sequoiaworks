c531278737aa0dd9df18850fcfdbf15001c35faa
sequoiaworks <sequoiaworks14@gmail.com>
2026-01-15 20:25:08 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -106,7 +106,7 @@ const Navbar: React.FC = () => {
             className="cursor-pointer flex items-center gap-2 z-[110]"
           >
           <img
-            src="/logo/Colour logo(1)(1).png"
+            src="Colour logo.pngg"
             alt="Sequoia Logo"
             className="w-8 h-8 object-contain"
           />
